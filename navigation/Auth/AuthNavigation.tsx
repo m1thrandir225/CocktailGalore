31d12b1350459715aba9e09bc4dbf391dcf26c67
Sebastijan Zindl <sebastijan32@hotmail.com>
2022-12-19 22:35:59 +0100

    Transistions added

--- navigation/Auth/AuthNavigation.tsx
+++ navigation/Auth/AuthNavigation.tsx
@@ -5,11 +5,20 @@ import { AuthStackParamList } from "../navigationTypes";
 import LoginScreen from "../../screens/Auth/LoginScreen";
 import SignUpScreen from "../../screens/Auth/SignUpScreen";
 import WelcomeNavigation from "../Welcome/WelcomeNavigation";
-const AuthStack = createNativeStackNavigator<AuthStackParamList>();
+import {
+  TransitionPresets,
+  createStackNavigator,
+} from "@react-navigation/stack";
+const AuthStack = createStackNavigator<AuthStackParamList>();
 
 const AuthNavigation = () => {
   return (
-    <AuthStack.Navigator screenOptions={{ headerShown: false }}>
+    <AuthStack.Navigator
+      screenOptions={{
+        headerShown: false,
+        ...TransitionPresets.ModalSlideFromBottomIOS,
+      }}
+    >
       <AuthStack.Screen name="Login" component={LoginScreen} />
       <AuthStack.Screen name="SignUp" component={SignUpScreen} />
     </AuthStack.Navigator>
--- navigation/Welcome/WelcomeNavigation.tsx
+++ navigation/Welcome/WelcomeNavigation.tsx
@@ -1,15 +1,23 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as React from "react";
 import WelcomeScreen from "../../screens/Welcome/WelcomeScreen";
+import {
+  TransitionPresets,
+  createStackNavigator,
+} from "@react-navigation/stack";
 import { WelcomeStackParamList } from "../navigationTypes";
 import AuthNavigation from "../Auth/AuthNavigation";
-const WelcomeStack = createNativeStackNavigator<WelcomeStackParamList>();
+
+const WelcomeStack = createStackNavigator<WelcomeStackParamList>();
 
 const WelcomeNavigation = () => {
   return (
     <WelcomeStack.Navigator
       initialRouteName="Welcome"
-      screenOptions={{ headerShown: false }}
+      screenOptions={{
+        headerShown: false,
+        ...TransitionPresets.SlideFromRightIOS,
+      }}
     >
       <WelcomeStack.Screen name="Welcome" component={WelcomeScreen} />
       <WelcomeStack.Screen name="Auth" component={AuthNavigation} />
