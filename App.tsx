3410ee1a7295faff5c49d257addd72b3ec9e225a
Sebastijan Zindl <sebastijan32@hotmail.com>
2022-12-22 22:22:36 +0100

    whole redesign of the app, clean files

--- App.tsx
+++ App.tsx
@@ -1,18 +1,6 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import Navigation from "./navigation";
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
-      <Navigation />
-    </SafeAreaProvider>
-  );
+  return <SafeAreaProvider></SafeAreaProvider>;
 }
-
-const styles = StyleSheet.create({
-  container: {},
-});
