5f36876560dab0858702f4a96f8a0b95d267bab0
Sebastijan Zindl <sebastijan32@hotmail.com>
2022-12-22 22:29:05 +0100

    global colors added

---
+++ constants/globalStyles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const AlmostDark = "#1B1B1B";
+export const RedLight = "#A7003C";
+export const RedDark = "#893040";
+export const GreenLight = "#41A700";
+export const GreenDark = "#308941";
+export const AlmostWhite = "#FAF8FF";
+
+export const globalStyles = StyleSheet.create({});
