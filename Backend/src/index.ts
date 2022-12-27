bb2c490a983ac393bbf4dfc0d5b0ecd81262db93
Sebastijan Zindl <sebastijan32@hotmail.com>
2022-12-27 23:21:05 +0100

    user routes added

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/users", usersRouter);
+app.use("/api/users/", usersRouter);
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
--- Backend/src/users/users.router.ts
+++ Backend/src/users/users.router.ts
@@ -1,9 +1,61 @@
+import * as UserService from "./users.methods";
 import express from "express";
 import type { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 
 export const usersRouter = express.Router();
 
-usersRouter.get("/", async (req: Request, res: Response) => {
-  res.send("Hello World!");
+//getUser
+usersRouter.get("/user/:id", async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id, 10);
+  try {
+    const user = await UserService.getUser(id);
+    if (user) {
+      return res.status(200).json(user);
+    }
+    return res.status(404).json("User not found");
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
 });
+
+//getAllUsers
+usersRouter.get("/all", async (req: Request, res: Response) => {
+  try {
+    const users = await UserService.getAllUsers();
+    if (users) {
+      return res.status(200).json(users);
+    }
+    return res.status(404).json("No users found");
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
+});
+
+//deleteUser
+usersRouter.delete("/user/:id", async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id, 10);
+  try {
+    await UserService.deleteUser(id);
+    return res.status(200).json("User deleted");
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
+});
+
+//getUserFavoriteCocktails
+usersRouter.get(
+  "/user/:email/favorites",
+  async (req: Request, res: Response) => {
+    const email = req.params.email;
+    try {
+      const favorites = await UserService.getUserFavoriteCocktails(email);
+      if (favorites) {
+        return res.status(200).json(favorites);
+      }
+      return res.status(404).json("No favorites found");
+    } catch (error: any) {
+      return res.status(500).json(error.message);
+    }
+  },
+);
