e8da62363f707ed8d0452608e401823f3840ae89
Sebastijan Zindl <sebastijan32@hotmail.com>
2022-12-29 13:01:45 +0100

    reset routes - wanted to do auth first then the rest

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,12 +1,6 @@
-import { flavourRouter } from "./flavour/flavour.router";
-import { authorRouter } from "./author/author.router";
-import { insightsRouter } from "./insights/insights.router";
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import { usersRouter } from "./users/users.router";
-import { cocktailsRouter } from "./cocktails/cocktails.router";
-import { authRouter } from "./auth/auth.router";
 dotenv.config();
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
@@ -15,14 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/users/", usersRouter); //users router
-app.use("/api/cocktails/", cocktailsRouter); //cocktails router
-app.use("/api/insights/", insightsRouter); //insights router
-app.use("/api/authors/", authorRouter); //authors router
-app.use("/api/flavours/", flavourRouter); //flavours router
-app.use("/", authRouter); //authorization router
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
-
