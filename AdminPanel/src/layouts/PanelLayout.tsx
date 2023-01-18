55c1cd6083e8a009f21c1c83ce8f21c178240e9c
Sebastijan Zindl <sebastijan32@hotmail.com>
2023-01-18 11:54:45 +0100

    import changes

--- AdminPanel/src/layouts/PanelLayout.tsx
+++ AdminPanel/src/layouts/PanelLayout.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Sidebar from "../components/global/Sidebar";
+import Sidebar from "@/components/global/Sidebar";
 
 function PanelLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-row justify-start items-start">
+    <div className="flex flex-row items-start justify-start">
       <Sidebar />
       {children}
     </div>
