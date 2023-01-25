4cd1e4a6745b8a8fff2ff567e6591abb724dc590
Sebastijan Zindl <sebastijan32@hotmail.com>
2023-01-25 16:42:23 +0100

    use external api instead of local

--- admin_panel/src/lib/axios-interceptor.ts
+++ admin_panel/src/lib/axios-interceptor.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:4000"
-      : "https://galore-cocktails-more-production.up.railway.app/",
+  baseURL: "https://galore-cocktails-more-production.up.railway.app/",
   headers: {
     "Content-Type": "application/json",
   },
