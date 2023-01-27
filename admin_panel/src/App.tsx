6ffb52fe0c25024faabdb1dda52f603bc9606344
Sebastijan Zindl <sebastijan32@hotmail.com>
2023-01-27 09:23:53 +0100

    hash router instead of browser router

--- admin_panel/src/App.tsx
+++ admin_panel/src/App.tsx
@@ -7,10 +7,19 @@ import { Routes, Route } from "react-router-dom";
 import ProtectedLayout from "./Layouts/ProtectedLayout";
 import Login from "./Login";
 import Dashboard from "./pages/Dashboard";
+import Cocktails from "./pages/Cocktails";
+import AddFlavour from "./pages/Flavours/AddFlavour";
+import EditFlavour from "./pages/Flavours/EditFlavour";
+import Flavours from "./pages/Flavours/Flavours";
+import Insights from "./pages/Insights";
+import Settings from "./pages/Settings";
+import EditUser from "./pages/Users/EditUser";
+import Users from "./pages/Users/Users";
 
 function App() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route
         path="/"
         element={
@@ -21,7 +30,86 @@ function App() {
           </RequireAuth>
         }
       />
-      <Route path="/login" element={<Login />} />
+      <Route
+        path="/cocktails"
+        element={
+          <RequireAuth loginPath="/login">
+            <ProtectedLayout>
+              <Cocktails />
+            </ProtectedLayout>
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/insights"
+        element={
+          <RequireAuth loginPath="/login">
+            <ProtectedLayout>
+              <Insights />
+            </ProtectedLayout>
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/flavours"
+        element={
+          <RequireAuth loginPath="/login">
+            <ProtectedLayout>
+              <Flavours />
+            </ProtectedLayout>
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/flavours/new"
+        element={
+          <RequireAuth loginPath="/login">
+            <ProtectedLayout>
+              <AddFlavour />
+            </ProtectedLayout>
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/flavours/flavour/:id"
+        element={
+          <RequireAuth loginPath="/login">
+            <ProtectedLayout>
+              <EditFlavour />
+            </ProtectedLayout>
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/users"
+        element={
+          <RequireAuth loginPath="/login">
+            <ProtectedLayout>
+              <Users />
+            </ProtectedLayout>
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/users/user/:id"
+        element={
+          <RequireAuth loginPath="/login">
+            <ProtectedLayout>
+              <EditUser />
+            </ProtectedLayout>
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/settings"
+        element={
+          <RequireAuth loginPath="/login">
+            <ProtectedLayout>
+              <Settings />
+            </ProtectedLayout>
+          </RequireAuth>
+        }
+      />
     </Routes>
   );
 }
--- admin_panel/src/main.tsx
+++ admin_panel/src/main.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { AuthProvider } from "react-auth-kit";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import ThemeProvider from "./context/ThemeContext";
+import { SWRConfig } from "swr";
+import axiosInstance from "./lib/axios-interceptor";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AuthProvider authType="localstorage" authName="_auth">
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ThemeProvider>
+        <SWRConfig
+          value={{
+            fetcher: (url) => axiosInstance.get(url).then((res) => res.data),
+          }}
+        >
+          <HashRouter>
+            <App />
+          </HashRouter>
+        </SWRConfig>
+      </ThemeProvider>
     </AuthProvider>
   </React.StrictMode>,
 );
