cf9795bfed0b4be2c2d94f88bacf86f362ad6806
Sebastijan Zindl <sebastijan32@hotmail.com>
2023-01-27 01:00:10 +0100

    removed react strict mode

--- admin_panel/src/main.tsx
+++ admin_panel/src/main.tsx
@@ -6,11 +6,9 @@ import { AuthProvider } from "react-auth-kit";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <AuthProvider authType="localstorage" authName="_auth">
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
-  </React.StrictMode>,
+  <AuthProvider authType="localstorage" authName="_auth">
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>,
 );
